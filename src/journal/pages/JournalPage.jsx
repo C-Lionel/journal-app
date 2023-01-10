8f7099760b6e2a7a0df60e38fb06363c7d02391a
lionel cancellieri <lio2014vt@gmail.com>
2023-01-10 00:10:12 -0300

    updated 10/1/23 00:10hs: PC-ESCRITORIO

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,16 @@
 import { Typography } from "@mui/material";
-// import { MailOutline } from '@mui/icons-material';
+import { JournalLayout } from "../layout/JournalLayout";
+
 
 export const JournalPage = () => {
   return (
-    <>
+    <JournalLayout>
 
-      <Typography variant='h1'> JournalPage </Typography>
-      {/* <MailOutline /> */}
+      <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero porro, commodi, cum corrupti velit id laboriosam necessitatibus et eos, quidem officia ratione. Nisi eos veritatis iure deserunt. Voluptatum, praesentium numquam. </Typography>
 
-    </>
+      {/* NothinSelected */}
+      {/* NoteView */}
+
+    </JournalLayout>
   )
 }
