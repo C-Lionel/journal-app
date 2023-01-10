0383f5db3bade284bdaa0d6ea47c22d2eb914e9d
lionel cancellieri <lio2014vt@gmail.com>
2023-01-10 17:49:00 -0300

    updated 10/1/23 17:50hs: PC-ESCRITORIO

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,19 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero porro, commodi, cum corrupti velit id laboriosam necessitatibus et eos, quidem officia ratione. Nisi eos veritatis iure deserunt. Voluptatum, praesentium numquam. </Typography>
+      {/* <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero porro, commodi, cum corrupti velit id laboriosam necessitatibus et eos, quidem officia ratione. Nisi eos veritatis iure deserunt. Voluptatum, praesentium numquam. </Typography> */}
+
 
       {/* NothinSelected */}
+      {/* <NothingSelectedView /> */}
+
       {/* NoteView */}
+      <NoteView />
 
     </JournalLayout>
   )
