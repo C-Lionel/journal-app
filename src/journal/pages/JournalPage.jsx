11e674cf617e7de44437932c5b67a540585dba85
lionel cancellieri <lio2014vt@gmail.com>
2023-01-11 19:19:20 -0300

    aplicación terminada

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 
@@ -10,10 +12,25 @@ export const JournalPage = () => {
 
 
       {/* NothinSelected */}
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
       {/* NoteView */}
-      <NoteView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+
+      </IconButton>
 
     </JournalLayout>
   )
